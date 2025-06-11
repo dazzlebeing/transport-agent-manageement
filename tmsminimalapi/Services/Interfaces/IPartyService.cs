42226df71de0c07a5f480737b4eddbfe8f4f7968
dazzlebeing <sandeeprajapat0000@gmail.com>
2025-06-11 09:23:11 +0530

    feat: implement party service interface

--- tmsminimalapi/Services/Interfaces/IPartyService.cs
+++ tmsminimalapi/Services/Interfaces/IPartyService.cs
@@ -1,1 +1,10 @@
- 
+using tmsminimalapi.DTOs;
+
+namespace tmsminimalapi.Services.Interfaces
+{
+    public interface IPartyService
+    {
+        Task<PartyResponseDTO> CreatePartyAsync(PartyCreateDTO partyDto);
+        Task<IEnumerable<PartySearchDTO>> SearchPartiesAsync(string query);
+    }
+} 
